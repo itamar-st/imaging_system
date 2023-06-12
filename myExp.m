9e349789cb936fb3a4ab88f8649fddb9c39b6626
itamar-st <81354824+itamar-st@users.noreply.github.com>
2023-06-12 16:14:38 +0300

    fix output. removed the listener

--- myExp.m
+++ myExp.m
@@ -53,30 +53,31 @@ function vr = initializationCodeFun(vr)
     vr.ao.IsContinuous = true;
     
     addAnalogInputChannel(vr.ai,'Dev1',[8,9,11],'Voltage');
-    addAnalogOutputChannel(vr.ao,'Dev1',[0,1],'Voltage');
+    addAnalogOutputChannel(vr.ao,'Dev1',1,'Voltage');
     % define the sampling rate to 1kHz and set the duration to be unlimited
     vr.ai.Rate = vr.rate;
     vr.ao.Rate = vr.rate;
 
             
-    initialData = zeros(vr.rate/10, 2);
+    initialData = zeros(vr.rate,1);
+%     initialData = ones(vr.rate / 10,1)*4;
     vr.ao.queueOutputData(initialData);
 
     % set the buffering window to be 8 ms long - shorter than a single ViRMEn refresh cycle
     vr.ai.NotifyWhenDataAvailableExceeds = vr.rate / 100;
-    vr.ao.NotifyWhenScansQueuedBelow = vr.rate/10;
+    vr.ao.NotifyWhenScansQueuedBelow  = vr.rate/10;
 
     vr.timeOfSample = vr.ai.Rate./vr.ai.NotifyWhenDataAvailableExceeds;
     vr.timePerSample = vr.ai.NotifyWhenDataAvailableExceeds./vr.ai.Rate;
     %radius of 9.525 cm for the wheel.
     vr.lh1 = addlistener(vr.ai,'DataAvailable',@getData);
-    vr.lh2 = addlistener(vr.ao,'DataRequired',@sendData);
+%     vr.lh2 = addlistener(vr.ao,'DataRequired',@sendData);
     function getData(src,event)
         dataFromDAQ = [event.Data event.TimeStamps];
     end
 
     function sendData(src,event)
-        zeros_columns = zeros(vr.rate / 10, 2);
+        zeros_columns = zeros(vr.rate / 10,1);
         src.queueOutputData(zeros_columns);
     end
 
@@ -92,8 +93,6 @@ function vr = initializationCodeFun(vr)
     % start acquisition from the analog input object
     startBackground(vr.ai);
     startBackground(vr.ao);
-
-
 end
 
 
@@ -125,19 +124,19 @@ function vr = runtimeCodeFun(vr)
 %         queueOutputData(vr.ai,result_matrix);
 %     end
     
-    %sound
+    %sound -------- return it
+    
     switch vr.soundProfile
         case 1
-           vr = soundInRangeA(vr, vr.targetSpeed, vr.allowedDeviation,1000);
+            vr = soundInRangeA(vr, vr.targetSpeed, vr.allowedDeviation,1600);
         case 2
-            %vr = soundTestgpt(vr, 1500)
             vr = soundEqualB(vr, vr.targetSpeed, vr.allowedDeviation,1000);
-            %soundEqualB(vr, 230,500);
         case 3
-            vr = soundDiffC(vr, vr.targetSpeed, vr.allowedDeviation,500);
+            vr = soundDiffC(vr, vr.targetSpeed, vr.allowedDeviation,2000);
         otherwise
             disp('ERROR myExp')
     end
+    disp(vr.ao.ScansQueued)
     
     %log data
     % obtain the current timestamp
@@ -163,6 +162,7 @@ function vr = runtimeCodeFun(vr)
     % Create the timer object
     % Start the timer object
     if (vr.position(2)>=125)
+        %vr = stopSound(vr); ---------- return it
         if (isequal(get(vr.t1, 'Running'), 'off'))
             start(vr.t1);
         end
@@ -172,8 +172,9 @@ function vr = runtimeCodeFun(vr)
             %check which room
             if (timeUntilCoolOffRoom == 1)
                 vr.currentWorld = 2;
-                    start(vr.t2);
-                    vr = giveReward(vr);
+                start(vr.t2);
+                vr = giveReward(vr);
+               
             else
                 %go back to init position
                 vr.currentWorld = vr.chosenWorld;
@@ -199,14 +200,15 @@ function vr = terminationCodeFun(vr)
     delete(vr.t1);
     delete(vr.t2);
     delete(vr.lh1);
-    %delete(vr.lh2);
+%     delete(vr.lh2);
     % stop the DAQ session
     stop(vr.ai);
+    stop(vr.ao);
     % close the file
     fclose(vr.fid1);
     fclose(vr.fid2);
     vr = logData(vr);
-
+    stopSound()
     % delete the temporary log file
-    %delete(vr.tempfile);
+%     delete(vr.tempfile);
 end
