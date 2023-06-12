a5af91bc3fcf26302037fc935482c9221ff74bf9
itamar-st <81354824+itamar-st@users.noreply.github.com>
2023-06-12 20:19:13 +0300

    logging from movewithDAQ

--- functions/logData.m
+++ functions/logData.m
@@ -1,19 +1,13 @@
-function vr = logData(vr)
-    global dataFromDAQ;
-    % obtain the current timestamp
-    timestamp = clock;
-    
-    % write timestamp and the x & y components of position and velocity to a file
-    % using floating-point precision   
-    fwrite(vr.fid1, [timestamp(6) vr.velocity(2)],'double');
-    
-    r1 = 0:(vr.ai.NotifyWhenDataAvailableExceeds/2)-1;
-    %if (isempty(dataFromDAQ) == 0)
-    dataFromDAQAEven = dataFromDAQ(:,1);
-    dataFromDAQBEven = dataFromDAQ(:,2);
-    matrix = [r1+250*vr.counter ;dataFromDAQAEven(2:2:end).';dataFromDAQBEven(2:2:end).'] ;
+function vr = logData(vr,Acol,Bcol,velocity,timestampCol)
+    %log velocity
+%     timestamp = clock;
+    fwrite(vr.fid1, [timestampCol(1) velocity(2)],'double');
+
+    %log a,b
+%     r1 = 0:(vr.ai.NotifyWhenDataAvailableExceeds/2)-1;
+    matrix = [timestampCol(2:2:end).';Acol(2:2:end).';Bcol(2:2:end).'] ;
     fwrite(vr.fid2, matrix,'double');
-    vr.counter = vr.counter + 1;
+
 
 end
 
