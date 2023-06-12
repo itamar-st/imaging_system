561d8c1144442b2ab03425bd7caebae9f6822a0b
itamar-st <81354824+itamar-st@users.noreply.github.com>
2023-06-12 20:18:19 +0300

    x axis data is now from the DAQ + move logging to seperate file

--- moveWithDAQ.m
+++ moveWithDAQ.m
@@ -1,47 +1,37 @@
 function velocity = moveWithDAQ(vr)
     global dataFromDAQ;
-    global shiftCounter;
+    %if(isempty(dataFromDAQ))
+     %  velocity = [0 0 0 0];
+     %   return
+    %end
     counter = 0;
-    dataFromDAQ;
-    direction = runDirectiongpt(dataFromDAQ);
+    %direction = runDirectiongpt(dataFromDAQ);
+    direction = 1;
     Acol = dataFromDAQ(:,1);
     Bcol = dataFromDAQ(:,2);
+    timestampCol = dataFromDAQ(:,4);
     length = numel(Bcol);
     for i = 2:length
         tmp = Bcol(i-1) + Bcol(i);
-        if tmp>3 & tmp < 6
+        if tmp>3 && tmp < 6
             counter = counter +1;
         end
     end
-    angle = (counter/1024)*360;
+    % extract the speed from the encoder
+    angle = (counter/1024)*360; % 1024 slits in the encoder
     angularSpeed = angle ./ double(vr.timeOfSample);
-    speed = angularSpeed*0.09525;
+    speed = angularSpeed*0.09525; % radius of 9.525 cm for the wheel.
     %diff = data - lastDataFromDAQ;
-    velocity = [0 counter*direction 0 0];
+    % 10000 = 100 for 10 ms to sec * 100 for cm to meter => velocity in m\s
+    scaling = 10000;
+    velocity = [0 speed*scaling*direction 0 0]; 
+%     velocity = [0 20 0 0];
     %calculate distance- 1024 slots / 360 deg = 2.844 slots per deg
     rotationAngle = counter*2.844;
     
-    
-    %log velocity
-    timestamp = clock;
-    fwrite(vr.fid1, [timestamp(6) velocity(2)],'double');
-    
-    
-    %log a,b
-    r1 = 0:(vr.ai.NotifyWhenDataAvailableExceeds/2)-1;
-    matrix = [r1+250*shiftCounter ;Acol(2:2:end).';Bcol(2:2:end).'] ;
-    fwrite(vr.fid2, matrix,'double');
-    shiftCounter = shiftCounter + 1
-    
-    
-    
-    
-    
+    vr = logData(vr,Acol,Bcol, velocity,timestampCol);
+ 
     if (vr.position(2)>=125) 
         velocity(2) = 0;
-        disp("position move " + vr.position(2))
-
     end
-    disp("velocity" + vr.velocity(2))
-
 end
