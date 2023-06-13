e4c04eb30d93fdbd14e9c9645c70dddc44dc41b1
itamar-st <81354824+itamar-st@users.noreply.github.com>
2023-06-13 20:54:00 +0300

    reward acording to duration

--- functions/giveReward.m
+++ functions/giveReward.m
@@ -1,8 +1,9 @@
 function vr = giveReward(vr)
-    data = ones(vr.rate/10,1)*4; % open valve for sec/10
+    data = ones(vr.rate*vr.rewardDuration/1000,1)*4; % open valve for sec/10
     zero_data = zeros(vr.rate/10,1);
     vr.ao.queueOutputData(data);
     vr.ao.queueOutputData(zero_data);
     startBackground(vr.ao);
+    vr.rewardOn = 1;
 
 end
