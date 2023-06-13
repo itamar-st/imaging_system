9e03b8ca56d6c58f36729ba4df5370edbb00f210
itamar-st <81354824+itamar-st@users.noreply.github.com>
2023-06-13 21:53:54 +0300

    sound boundaries changed, randomizing world, new GUI

--- myExp.m
+++ myExp.m
@@ -16,18 +16,27 @@ end
 % --- INITIALIZATION code: executes before the ViRMEn engine starts.
 function vr = initializationCodeFun(vr)
     
-    global dataFromDAQ;
     global timeUntilCoolOffRoom;
     
     %vr.trackLength = eval(vr.exper.variables.trackLength)
     vr.exper.variables.x = '200';
     timeUntilCoolOffRoom = 0;
     vr.counter = 0;
+    
     vr.timeOfRanningInRange = 0;
     vr.timeOfTotalRun = 0;
-    vr.precentageOfRunningInRange = 0.7;
-    vr = miniGUI(vr); % show the GUI for chosing the experiment preferences 
-    vr.currentWorld = vr.chosenWorld; %show the world we chose in the GUI
+    %counting amount of finished trials in a session.
+    vr.countTrials = 0;
+    vr = miniGUI(vr); % show the GUI for chosing the experiment preferences
+    %choose world, return line 27 if you want to control it via GUI and
+    %comment lines 29-34
+    %vr.currentWorld = vr.chosenWorld; %show the world we chose in the GUI
+    randomNumber = rand
+    if (randomNumber > 0.5)
+        vr.currentWorld = 1;
+    else
+        vr.currentWorld = 3;
+    end
     
     vr = timerInit(vr); % start the timers for changing rooms
     
@@ -47,17 +56,18 @@ function vr = runtimeCodeFun(vr)
         case 1
             vr = soundInRangeA(vr, vr.targetSpeed, vr.allowedDeviation,1600);
         case 2
-            vr = soundEqualB(vr, vr.targetSpeed, vr.allowedDeviation,1000);
+            vr = soundEqualB(vr, vr.targetSpeed, vr.allowedDeviation, vr.DeviationBetweenSteps, 1000);
         case 3
-            vr = soundDiffC(vr, vr.targetSpeed, vr.allowedDeviation,2000);
+            vr = soundDiffC(vr, vr.targetSpeed, vr.allowedDeviation,vr.DeviationBetweenSteps,2000);
         otherwise
-            disp('ERROR myExp')
+            disp('ERROR selecting sound profile')
     end
     
     %checking position for reward
     if (vr.position(2)>=125)
         vr = endOfTraceProcedure(vr);
     end
+    
 end
 
 % --- TERMINATION code: executes after the ViRMEn engine stops.
